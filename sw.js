19358aaa9229eb6fb34f2fa7fcea2806d31939fe
Klay <vitormiguelrodrigues79@gmail.com>
2026-09-03 18:49:58 +0100

    Refresh PWA to v2 and show current/next workout plans

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE='rgrd-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='rgrd-v2';
+const ASSETS=['./app-v2.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch('./app-v2.html',{cache:'no-store'}).catch(()=>caches.match('./app-v2.html')));
+    return;
+  }
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
