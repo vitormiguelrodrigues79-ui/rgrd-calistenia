13c85b567adad83a0713e7ff20b985570fb42ee2
Klay <vitormiguelrodrigues79@gmail.com>
2026-09-03 18:56:48 +0100

    Force PWA refresh and clear stale iPhone cache

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
-const CACHE='rgrd-v2';
-const ASSETS=['./app-v2.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
-self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){
-    e.respondWith(fetch('./app-v2.html',{cache:'no-store'}).catch(()=>caches.match('./app-v2.html')));
+const CACHE='rgrd-v3';
+const APP='./app-v2.html?v=3';
+const ASSETS=[APP,'./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(APP,{cache:'no-store'}).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(APP,copy));
+      return response;
+    }).catch(()=>caches.match(APP)));
     return;
   }
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+  event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
 });
